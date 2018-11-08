238f1984115dd05c9eb2a23ce2a3313171403ee1
Arne Libbrecht <arne.libbrecht@icloud.com>
2018-11-08 09:14:02 +0100

    Initial Commit

---
+++ Opdracht5-Galgje/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Opdracht5-Galgje
+//
+//  Created by student on 08/11/2018.
+//  Copyright © 2018 Vives. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
